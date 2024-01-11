cf7a1de0f7681433b2c1da61ced5a1342f62ff3a
Nancy <nhoushop@outlook.com>
2024-01-11 04:52:00 -0500

    Added Logout button and connected Login page to DB

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,15 +1,43 @@
-import { useState } from "react"
 import { FaSignInAlt } from "react-icons/fa"
+import { useSelector, useDispatch } from "react-redux"
+;
 import { useNavigate } from "react-router-dom";
+import {toast} from 'react-toastify';
+import { login, reset } from '../features/auth/authSlice';
+import { useEffect } from "react";
+import Spinner from '../components/Spinner'
 
 function Login() {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const {user, isLoading, isError, isSuccess, message} = useSelector((state) => state.auth)
+
+    useEffect(() => {
+        if(isError) {
+            toast.error(message)
+        }
+
+        if(isSuccess){
+            navigate('/')
+        }
+        dispatch(reset())
+    }, [user, isError, isSuccess, message, navigate])
 
     function handleSubmit(event) {
         event.preventDefault();
         const fd = new FormData(event.target);
         const data = Object.fromEntries(fd.entries());        
+        const userData = {
+            email: data.email,
+            password: data.password
+        }
+        dispatch(login(userData));
     }
 
+    if(isLoading) {
+        return <Spinner />
+    }
   return (
     <>
         <section className="heading">
